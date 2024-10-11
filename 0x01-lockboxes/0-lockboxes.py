c941d2459291265ab99a078d62baa0445db7a974
ermi4sol <ermi4sol@gmail.com>
2024-10-11 13:28:37 +0300

    fix bug

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains the `canUnlockAll` function which determines if all boxes
+in a list of lists can be unlocked using the keys found within the boxes.
+"""
+
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be unlocked.
+
+    Args:
+        boxes: A list of lists, where each sublist represents the keys in that box.
+
+    Returns:
+        True if all boxes can be unlocked, False otherwise.
+    """
     # Number of boxes
     n = len(boxes)
     
@@ -23,8 +38,21 @@ if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
     print(canUnlockAll(boxes))  # True
 
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    boxes = [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]
     print(canUnlockAll(boxes))  # True
 
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # False
+    boxes = [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[0]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[10, 3, 8, 9, 6, 5, 8, 1], [8, 5, 3, 7, 1, 8, 6], [5, 1, 9, 1], [], 
+             [6, 6, 9, 4, 3, 2, 3, 8, 5], [9, 4], [4, 2, 5, 1, 1, 6, 4, 5, 6], 
+             [9, 5, 8, 8], [6, 2, 8, 6]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3], 
+             [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7], 
+             [4, 2, 9, 6, 6, 5, 5]]
+    print(canUnlockAll(boxes))  # True
